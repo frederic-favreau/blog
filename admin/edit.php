b0d99cea02fa613b1bb334d75e07124fce7039cd
frederic-favreau <frederic.favreau22@gmail.com>
2023-02-25 03:22:37 +0100

    ajout de la section edition

--- admin/edit.php
+++ admin/edit.php
@@ -1,34 +1,54 @@
 <?php
-include_once './header-main.php';
 require_once '../connexion.php';
+include_once './header-main.php';
 ?>
-    <section>
-            <h1>Création d'article</h1>
-            <form action="#" method="POST">
-                <fieldset>
-                    <legend>Ton futur article</legend>
-                    <div>
-                        <label for="title">Titre de ton article</label>
-                        <input type="text" name="title" id="title" maxlength="50">
-                    </div>
-                    <div>
-                        <label for="extract">Description de ton article</label>
-                        <textarea name="extract" id="extract" cols="90" rows="10"></textarea>
-                    </div>
-                    <div>
-                        <label for="content">Contenu de ton poste</label>
-                        <textarea name="content" id="content" cols="120" rows="20" maxlength="4000"></textarea>
-                    </div>
-                    <div>
-                        <label for="author">Auteur</label>
-                        <input type="text" name="author" id="author" maxlength="50">
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Reset / Poster</legend>
-                    <input class="btn-form" type="reset" name="reset" value="Reset">
-                    <input class="btn-form" type="submit" name="submit" value="Poster">
-                </fieldset>
-            </form>
-        </section>
-    </main>
+
+<section id="section-edition">
+
+    <?php
+    $id = $_GET['id'];
+    if (isset($_POST['submit'])) {
+        $title = addslashes($_POST['title']);
+        $extract = addslashes($_POST['extract']);
+        $thumbnail = 'monimage.jpg';
+        $content = addslashes($_POST['content']);
+
+        $reqredit = $db->prepare("UPDATE `post` SET `title`='$title',`extract`='$extract',`content`='$content' WHERE `id` = :id");
+        $reqredit->bindParam('id', $id, PDO::PARAM_INT);
+        $reqredit->execute();
+    }
+    ?>
+
+    <?php
+
+    $reqedit = $db->prepare("SELECT `title`, `extract`,`content` FROM `post` WHERE `id` = :id");
+    $reqedit->bindParam('id', $id, PDO::PARAM_INT);
+    $reqedit->execute();
+    while ($edit = $reqedit->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+
+        <form action="#" method="POST">
+            <fieldset>
+                <legend>Ton futur article</legend>
+                <div>
+                    <label for="title">Titre de ton article</label>
+                    <input type="text" name="title" id="title" maxlength="50" value="<?= $edit['title'] ?>">
+                </div>
+                <div>
+                    <label for="extract">Description de ton article</label>
+                    <textarea name="extract" id="extract" cols="90" rows="10"><?= $edit['extract'] ?></textarea>
+                </div>
+                <div>
+                    <label for="content">Contenu de ton poste</label>
+                    <textarea name="content" id="content" cols="120" rows="20" maxlength="4000"><?= $edit['content'] ?></textarea>
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend>Reset / Poster</legend>
+                <input class="btn-form" type="reset" name="reset" value="Reset">
+                <input class="btn-form" type="submit" name="submit" value="Poster">
+            </fieldset>
+        </form>
+    <?php } ?>
+</section>
+</main>
